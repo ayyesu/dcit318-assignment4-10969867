6a6b092890ab2b43995bdeeb24c931b916e1b947
ayyesu <iamdanielkissiedu@gmail.com>
2024-07-06 18:20:13 +0000

    login Screen Aplication

---
+++ LoginScreenApplication/LoginScreenApplication/Form1.cs
@@ -0,0 +1,23 @@
+namespace LoginScreenApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            MessageBox.Show($"User with username {username} and password {password} logged in successfully!!");
+        }
+    }
+}
